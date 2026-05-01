73eed752c47a1758766e1a795bf5884e2d32a1ac
snehaisagoodgirl <ingawalesneha6@gmail.com>
2026-05-01 21:44:55 +0530

    Add momentum calculation script

---
+++ code1.py
@@ -0,0 +1,11 @@
+# calculateMomentum.py
+
+# Input
+mass = float(input())
+velocity = float(input())
+
+# Calculation
+momentum = mass * velocity
+
+# Output (rounded to 2 decimal places, no space)
+print(f"{momentum:.2f}kgm/s")
